faaf42bd9ec69c25c9ed66ff43f2d1156ee86ed2
MarginLove <65957261+MarginLove@users.noreply.github.com>
2021-11-17 23:44:49 +0800

    Update web.py

--- 2020广东省赛题/web.py
+++ 2020广东省赛题/web.py
@@ -1,17 +1,17 @@
-import urllib
-num = 1
-f = open("flag.txt","wb")
+import F1
+F5 = 1
+f = F2("flag.txt","wb")
 while num < 254:
     try:
-        ip = str(num)
+        F4 = str(num)
         print(ip)
         url="http://172.16."+ip+".250/flag.txt" 
         flag = urllib.urlopen(url).read()
-        f.write("172.16."+ip+".10  :  ")
-        f.write(flag)
-        f.write("\n")
+        f.F3("172.16."+ip+".10  :  ")
+        f.F3(flag)
+        f.F3("\n")
         print(flag)
         num = num + 1
-    except:
+    F6:
         num =num + 1
-f.close()
+f.F7()
